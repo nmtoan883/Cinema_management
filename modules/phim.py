5830841ca87fd6cde40132f8fbc8a29c61b08391
Hungvu25 <vhung9955@gmail.com>
2026-06-09 23:08:16 +0700

    update

--- modules/phim.py
+++ modules/phim.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_prefix
+from flask import Blueprint, render_template, request, redirect
 import database
 
 # Đăng ký Blueprint cho module Phim
